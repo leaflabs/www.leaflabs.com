c435777d0bb662349470080f6e96b5361144839a
bnewbold <bnewbold@robocracy.org>
2013-02-11 19:22:02 -0500

    front page

--- pages/blog.md
+++
@@ -1,6 +0,0 @@
-Title: Blog
-Date: 2010-04-02 21:11
-Author: Drews
-Category: Uncategorized
-
-
--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,7 @@ TIMEZONE = 'UTC'
 
 DEFAULT_LANG = u'en'
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 6
 
 ARTICLE_URL = '{date:%Y}/{date:%b}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%b}/{slug}/index.html'
@@ -20,18 +20,20 @@ PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
 PAGE_LANG_URL = '{lang}/{slug}/'
 PAGE_LANG_SAVE_AS = '{lang}/{slug}/index.html'
+INDEX_SAVE_AS = 'blog/index.html'
 
-AUTHOR_SAVE_AS = False
-CATEGORY_SAVE_AS = False
-TAG_SAVE_AS = False
+#AUTHOR_SAVE_AS = False
+#CATEGORY_SAVE_AS = False
+#TAG_SAVE_AS = False
 
 PATH = '.'
 ARTICLE_DIR = ('posts')
 PAGE_DIR = ('pages')
 #FILES_TO_COPY = (('style', 'style'), )
 STATIC_PATHS = ['style']
+TEMPLATE_PAGES = {'home.html': 'index.html'}
 
-#DIRECT_TEMPLATES = ('archives')
+DIRECT_TEMPLATES = ('index', 'archives')
 
 FEED_ALL_RSS = 'main.xml'
 FEED_MAX_ITEMS = '20'
